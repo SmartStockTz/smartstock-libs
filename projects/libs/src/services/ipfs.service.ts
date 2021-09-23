ebc63779e0110783c692b46f6545a79709e60f0e
joshua <mama27j@gmail.com>
2021-09-23 03:06:16 +0200

    remove ipfs usage

--- projects/libs/src/services/ipfs.service.ts
+++ projects/libs/src/services/ipfs.service.ts
@@ -2,56 +2,60 @@ export class IpfsService {
   private static instance;
 
   private static async ipfs(): Promise<any> {
-    if (!this.instance) {
-      // @ts-ignore
-      this.instance = await window.Ipfs.create();
-      return this.instance;
-    }
-    return this.instance;
+    // if (!this.instance) {
+    //   // @ts-ignore
+    //   this.instance = await window.Ipfs.create();
+    //   return this.instance;
+    // }
+    // return this.instance;
   }
 
   private constructor() {
   }
 
-  static async getDataFromCid<T>(cid: string): Promise<T> {
-    const node = await IpfsService.ipfs();
-    const results = await node.cat(cid, {
-      timeout: 1000 * 60 * 5,
-    });
-    // @ts-ignore
-    node.pin.add(window.Ipfs.CID.parse(cid)).catch(console.log);
-    let data = '';
-    for await (const chunk of results) {
-      data += chunk.toString();
-    }
-    const dt = JSON.parse(data);
-    if (dt?._id) {
-      dt.id = dt._id;
-      delete dt._id;
-    }
-    if (dt?._created_at) {
-      dt.createdAt = dt._created_at;
-      delete dt._created_at;
-    }
-    if (dt?._updated_at) {
-      dt.updatedAt = dt._updated_at;
-      delete dt._updated_at;
-    }
-    return dt;
+  static async getDataFromCid(cid: any): Promise<any> {
+    return cid;
+    // const node = await IpfsService.ipfs();
+    // const results = await node.cat(cid, {
+    //   timeout: 1000 * 60 * 5,
+    // });
+    // // @ts-ignore
+    // node.pin.add(window.Ipfs.CID.parse(cid)).catch(console.log);
+    // let data = '';
+    // for await (const chunk of results) {
+    //   data += chunk.toString();
+    // }
+    // const dt = JSON.parse(data);
+    // if (dt?._id) {
+    //   dt.id = dt._id;
+    //   delete dt._id;
+    // }
+    // if (dt?._created_at) {
+    //   dt.createdAt = dt._created_at;
+    //   delete dt._created_at;
+    // }
+    // if (dt?._updated_at) {
+    //   dt.updatedAt = dt._updated_at;
+    //   delete dt._updated_at;
+    // }
+    // return dt;
   }
 
   static async getId(): Promise<any> {
-    const node = await this.ipfs();
-    return await node.id();
+    // const node = await this.ipfs();
+    // return await node.id();
+    return 'n/a';
   }
 
   static async getVersion(): Promise<any> {
-    const node = await this.ipfs();
-    return await node.version();
+    // const node = await this.ipfs();
+    // return await node.version();
+    return 'n/a';
   }
 
   static async getStatus(): Promise<boolean> {
-    const node = await this.ipfs();
-    return node.isOnline();
+    // const node = await this.ipfs();
+    // return node.isOnline();
+    return true;
   }
 }

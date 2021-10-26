7539d4498983b55b1b039b3e0ecb595cb7316638
joshua <mama27j@gmail.com>
2021-10-26 07:51:33 +0300

    update syncs worker

--- projects/libs/src/workers/syncs.worker.ts
+++ projects/libs/src/workers/syncs.worker.ts
@@ -1,89 +1,109 @@
 import {expose} from 'comlink';
 import * as bfast from 'bfast';
-import {ShopModel} from '../models/shop.model';
-import {LibUserModel} from '../models/lib-user.model';
 
-export function getDaasAddress(shop: ShopModel): string {
-  return `https://smartstock-faas.bfast.fahamutech.com/shop/${shop.projectId}/${shop.applicationId}`;
-}
-
-export function getFaasAddress(shop: ShopModel): string {
-  return `https://smartstock-faas.bfast.fahamutech.com/shop/${shop.projectId}/${shop.applicationId}`;
-}
+// export function getDaasAddress(shop: ShopModel): string {
+//   return `https://smartstock-faas.bfast.fahamutech.com/shop/${shop.projectId}/${shop.applicationId}`;
+// }
+//
+// export function getFaasAddress(shop: ShopModel): string {
+//   return `https://smartstock-faas.bfast.fahamutech.com/shop/${shop.projectId}/${shop.applicationId}`;
+// }
 
 let isRunning = false;
 
-async function saveLocalDataToServer(projectId: string): Promise<any> {
-  const keys = await bfast.cache(null, projectId).getSyncsKeys();
-  // const datas = await bfast.cache(null, projectId).getAllSyncs();
-  for (const k of keys) {
-    const data = await bfast.cache(null, projectId).getOneSyncs(k);
-    if (data.action === 'delete') {
-      await bfast.database(projectId).tree(data.tree)
-        .query()
-        .byId(data.payload.id)
-        .delete({useMasterKey: true});
+async function saveLocalDataToServer(): Promise<any> {
+  const keys = await bfast.cache().getSyncsKeys();
+  const p1 = keys.map(async k => {
+    try {
+      const data = await bfast.cache().getOneSyncs(k);
+      if (data.projectId === undefined || data.projectId === null) {
+        await bfast.cache().removeOneSyncs(k);
+      }
+      if (data.applicationId === undefined || data.applicationId === null) {
+        await bfast.cache().removeOneSyncs(k);
+      }
+      if (data.databaseURL === undefined || data.databaseURL === null) {
+        await bfast.cache().removeOneSyncs(k);
+      }
+      if (data.action === 'delete') {
+        bfast.init({
+          projectId: data.projectId,
+          applicationId: data.applicationId,
+          databaseURL: data.databaseURL,
+          functionsURL: data.databaseURL
+        }, data.projectId);
+        await bfast.database(data.projectId).tree(data.tree)
+          .query()
+          .byId(data.payload.id)
+          .delete({useMasterKey: true});
+      }
+      if (data.action === 'update') {
+        await bfast.database(data.projectId).tree(data.tree)
+          .query()
+          .byId(data.payload.id)
+          .updateBuilder()
+          .upsert(true)
+          .doc(data.payload)
+          .update();
+      }
+      if (data.action === 'create') {
+        await bfast.database(data.projectId).tree(data.tree).save(data.payload);
+      }
+      await bfast.cache().removeOneSyncs(k);
+    } catch (e) {
+      console.log(e);
     }
-    if (data.action === 'create' || data.action === 'update') {
-      await bfast.database(projectId).tree(data.tree)
-        .query()
-        .byId(data.payload.id)
-        .updateBuilder()
-        .upsert(true)
-        .doc(data.payload)
-        .update();
-      // console.log(r, 'after update');
-    }
-    await bfast.cache(null, projectId).removeOneSyncs(k);
-  }
+  });
+  await Promise.all(p1);
 }
 
-function getShops(user: LibUserModel): ShopModel[] {
-  if (!user || !Array.isArray(user?.shops)) {
-    return [];
-  }
-  const shops = [];
-  user.shops.forEach(element => {
-    shops.push(element);
-  });
-  shops.push({
-    businessName: user.businessName,
-    projectId: user.projectId,
-    applicationId: user.applicationId,
-    // masterKey: user.masterKey,
-    projectUrlId: user.projectUrlId,
-    settings: user.settings,
-    ecommerce: user.ecommerce,
-    street: user.street,
-    country: user.country,
-    region: user.region
-  });
-  return shops;
-}
+// function getShops(user: LibUserModel): ShopModel[] {
+//   if (!user || !Array.isArray(user?.shops)) {
+//     return [];
+//   }
+//   const shops = [];
+//   user.shops.forEach(element => {
+//     shops.push(element);
+//   });
+//   shops.push({
+//     businessName: user.businessName,
+//     projectId: user.projectId,
+//     applicationId: user.applicationId,
+//     // masterKey: user.masterKey,
+//     projectUrlId: user.projectUrlId,
+//     settings: user.settings,
+//     ecommerce: user.ecommerce,
+//     street: user.street,
+//     country: user.country,
+//     region: user.region
+//   });
+//   return shops;
+// }
 
-async function init(): Promise<any> {
+async function run(): Promise<any> {
   bfast.init({
     applicationId: 'smartstock_lb',
     projectId: 'smartstock'
   });
-  const user = await bfast.auth().currentUser();
-  const shops = getShops(user as any);
-  if (Array.isArray(shops)) {
-    for (const shop of shops) {
-      if (shop && shop.applicationId && shop.projectId) {
-        bfast.init({
-          applicationId: shop.applicationId,
-          projectId: shop.projectId,
-          adapters: {
-            auth: 'DEFAULT'
-          },
-          databaseURL: getDaasAddress(shop),
-          functionsURL: getFaasAddress(shop),
-        }, shop.projectId);
-        await saveLocalDataToServer(shop.projectId);
-      }
-    }
-  }
+  await saveLocalDataToServer();
+  // const user = await bfast.auth().currentUser();
+  // const shops = getShops(user as any);
+  // if (Array.isArray(shops)) {
+  //   for (const shop of shops) {
+  //     if (shop && shop.applicationId && shop.projectId) {
+  //       bfast.init({
+  //         applicationId: shop.applicationId,
+  //         projectId: shop.projectId,
+  //         adapters: {
+  //           auth: 'DEFAULT'
+  //         },
+  //         databaseURL: getDaasAddress(shop),
+  //         functionsURL: getFaasAddress(shop),
+  //       }, shop.projectId);
+  //       await saveLocalDataToServer(shop.projectId);
+  //     }
+  //   }
+  // }
 }
 
 export class SyncsWorker {
@@ -91,7 +111,7 @@ export class SyncsWorker {
     setInterval(_1 => {
       if (isRunning === false) {
         isRunning = true;
-        init().then(_2 => {
+        run().then(_2 => {
           // console.log('done save for remove syncs');
         }).catch(reason => {
           console.log(reason);

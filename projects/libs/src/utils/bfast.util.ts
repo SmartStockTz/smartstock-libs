c64f06d7be19bcec4119828256a85d6e7de243d0
joshua <mama27j@gmail.com>
2021-11-03 10:06:50 +0100

    update faas url

--- projects/libs/src/utils/bfast.util.ts
+++ projects/libs/src/utils/bfast.util.ts
@@ -5,5 +5,5 @@ export function getDaasAddress(shop: ShopModel): string {
 }
 
 export function getFaasAddress(shop: ShopModel): string {
-  return `https://smartstock-faas.bfast.fahamutech.com/shop/${shop.projectId}/${shop.applicationId}`;
+  return `https://smartstock-faas.bfast.fahamutech.com`;
 }

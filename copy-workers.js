31bf4fa8601dcf5acb0491e811a98072b655bb78
joshua <mama27j@gmail.com>
2021-11-07 18:07:36 +0100

    update copy worker to support esm2020

--- copy-workers.js
+++ copy-workers.js
@@ -14,7 +14,26 @@ try {
     const dest = join(__dirname, 'dist/libs/workers/', w);
     copyFileSync(src, dest);
   });
-  console.log('done copying');
+  console.log('done copying esm2015');
+} catch (e) {
+  console.log(e.toString())
+}
+
+
+const wDir2 = join(__dirname, 'dist/libs/esm2020/workers/');
+try {
+  lstatSync(wDir2);
+  const workers = readdirSync(wDir2);
+  try {
+    mkdirSync(join(__dirname, 'dist/libs/fesm2020/workers/'));
+  } catch (_43) {
+  }
+  workers.forEach(w => {
+    const src = join(__dirname, 'dist/libs/esm2020/workers/', w);
+    const dest = join(__dirname, 'dist/libs/workers/', w);
+    copyFileSync(src, dest);
+  });
+  console.log('done copying esm2020');
 } catch (e) {
   console.log(e.toString())
 }
